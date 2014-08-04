45c6815ca1b1f7770b3484123b1b58a1c779470a
U-AP\luooofuq <luooofuq@WW300408.ap.tieto.com>
2014-08-04 11:20:57 +0800

    1. Fix a bug: if a file can't be opened, the program will die.

--- perl-grep.pl
+++ perl-grep.pl
@@ -9,12 +9,14 @@ my $file;
 foreach $file (@ARGV)
 {
    $file =~ s/^"(.*)"[\x0A\x0D]*$/$1/g;	#remove the surrounding "
-   open my $fh, '<', $file or die "unable to open file '$file' for reading : $!";
-
-   my $line_count = 0;
-   while (my $line = <$fh>){
-       $line_count++;
-       print $file, ':', $line_count, ':', $line if $line =~ $pattern;
+   if (open my $fh, '<', $file){
+       my $line_count = 0;
+       while (my $line = <$fh>){
+	   $line_count++;
+	   print $file, ':', $line_count, ':', $line if $line =~ $pattern;
+       }
+       close($fh);
+   } else {
+       print "Can not open '$file'\n"
    }
-   close($fh);
 }
