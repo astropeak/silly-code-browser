c0d89d22f9721a718b6c71d2ae796aed8792ac80
U-AP\luooofuq <luooofuq@WW300408.ap.tieto.com>
2014-08-21 16:54:17 +0800

    Test file.

---
+++ test.el
@@ -0,0 +1,16 @@
+;; Test tree
+(setq tree (tree-create))
+
+(tree-add-element tree tree-head-element "A")
+(tree-add-element tree tree-head-element "B")
+(tree-add-element tree tree-head-element "C")
+(tree-add-element tree "A" "AA")
+(tree-add-element tree "A" "AB")
+(tree-add-element tree "AB" "AAA")
+
+;; currently tree will be:
+;;(">ROOT_OF_TREE<" ("A" ("AA") ("AB" ("AAA"))) ("B") ("C"))
+
+;;(tree-move-subtree tree "AB" ">ROOT_OF_TREE<")
+;; Problem: if we move a element under a child of it, there will be wrong. There will be a ring. So we must ensure parent is not a decendent of element before moving.
+;;(tree-move-subtree tree "A" "AB")
